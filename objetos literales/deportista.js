5954090a0720f91b5aaca5df185869f4438ecc5d
Estebanngarcia <fantingar@gmail.com>
2023-02-06 19:42:34 -0300

    funcion deportista

--- objetos literales/deportista.js
+++ objetos literales/deportista.js
@@ -17,8 +17,17 @@ let deportista = {
     experiencia: 10,
     nombre: "Aimar",
     entrenarHoras:function(horas){
-        this.energia = this.horas;
+        this.energia = this.energia - horas*5;
+        this.experiencia = this.experiencia + horas*2;
     }
 };
 
-console.log(deportista.entrenarHoras(5))
+// console.log(deportista.entrenarHoras(2))
+console.log("==Antes de comenzar entrenamiento==");
+console.log("Deportista energia: "+deportista.energia);
+console.log("Deportista experiencia: "+deportista.experiencia);
+console.log("==ENTRENANDO==");
+deportista.entrenarHoras(5);
+console.log("==FIN ENTRENAMIENTO==");
+console.log("Deportista energia: "+deportista.energia);
+console.log("Deportista experiencia: "+deportista.experiencia);
